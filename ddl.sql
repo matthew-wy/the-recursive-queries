e2f2e3aaace5f12672ad46b74ef628a1420dba46
matthew-wy <matthew.wygal@proton.me>
2024-10-30 20:00:44 -0700

    Added sample data to all rows except Sales and SalesProducts

--- ddl.sql
+++ ddl.sql
@@ -73,5 +73,73 @@ CREATE TABLE IF NOT EXISTS ProductsLocations (
     ON UPDATE CASCADE
 );
 
+-- Inserting in values ---------------------------------------
+
+-- Categories
+INSERT INTO Categories (name)
+VALUES ("Health and Beauty"),
+("Snacks"),
+("Paper"),
+("Prepared Food"),
+("Drinks");
+
+-- Customers
+INSERT INTO Customers (name)
+VALUES ("Sam S"),
+("MJ Lee"),
+("Kate Cole"),
+("Abdul Smith"),
+("Sven Garcia");
+
+-- Products
+INSERT INTO Products (categoryId, name, unitPrice)
+VALUES ((SELECT categoryId FROM Categories WHERE name = 'Paper'), "Pocket Wet Tissues", 3.99),
+((SELECT categoryId FROM Categories WHERE name = 'Snacks'), 'Yum Shrimp Chips', 2.50),
+((SELECT categoryId FROM Categories WHERE name = 'Health and Beauty'), 'Travel Toothbrush Set', 5.00),
+((SELECT categoryId FROM Categories WHERE name = 'Prepared Food'), 'Healthy Chicken Rice Set', 8.25),
+((SELECT categoryId FROM Categories WHERE name = 'Drinks'), 'Super Sparkling Water', 2.00);
+
+-- Locations
+INSERT INTO Locations (aisle, shelf, slot, capacity)
+VALUES ('A', '1', '2', 2),
+('B', '2', '4', 1),
+('D', '2', '3', 4),
+('A', '5', '4', 3),
+('B', '4', '5', 2);
+
+-- Sales
+
+
+-- SalesProducts
+
+
+-- ProductsLocations
+INSERT INTO ProductsLocations (productId, locationId, quantity)
+VALUES (
+  (SELECT productId FROM Products WHERE name = 'Pocket Wet Tissues'),
+  (SELECT locationId FROM Locations WHERE locationId = 2),
+  3
+),
+(
+  (SELECT productId FROM Products WHERE name = 'Yum Shrimp Chips'),
+  (SELECT locationId FROM Locations WHERE locationId = 4),
+  6
+),
+(
+  (SELECT productId FROM Products WHERE name = 'Travel Toothbrush Set'),
+  (SELECT locationId FROM Locations WHERE locationId = 1),
+  4
+),
+(
+  (SELECT productId FROM Products WHERE name = 'Healthy Chicken Rice Set'),
+  (SELECT locationId FROM Locations WHERE locationId = 5),
+  8
+),
+(
+  (SELECT productId FROM Products WHERE name = 'Super Sparkling Water'),
+  (SELECT locationId FROM Locations WHERE locationId = 3),
+  12
+);
+
 SET FOREIGN_KEY_CHECKS=1;
 COMMIT;
